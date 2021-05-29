aefba3169a839a5d0b86b70580fadb9f977c84a5
bishal14 <Bishal Maharjan@ID00919.speedport.ip>
2021-05-29 18:10:46 +0200

    first contact class created.

---
+++ TestProjectt/src/Contact.java
@@ -0,0 +1,29 @@
+
+public class Contact {
+
+	private String name;
+	private String cellNumber;
+
+	public Contact(String name, String cellNumber) {
+		super();
+		this.name = name;
+		this.cellNumber = cellNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCellNumber() {
+		return cellNumber;
+	}
+
+	public void setCellNumber(String cellNumber) {
+		this.cellNumber = cellNumber;
+	}
+
+}
